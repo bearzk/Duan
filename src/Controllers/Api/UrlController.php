1cd719f7d92792c5a65c7824679da0873b176ee1
Kai Zhang <kai.zhangxa@gmail.com>
2016-09-23 15:47:25 +0200

    Use correct header
    thank to dredd test

--- src/Controllers/Api/UrlController.php
+++ src/Controllers/Api/UrlController.php
@@ -1,10 +1,12 @@
 <?php
 namespace Duan\Controllers\Api;
 
+use Cicada\Routing\Router;
 use Duan\DuanApp;
 use Duan\Models\Url;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UrlController
 {
@@ -20,7 +22,12 @@ class UrlController
         $params = (array) json_decode($request->getContent());
         $url = Url::create($params);
 
-        return new JsonResponse($url);
+        /** @var Router $router */
+        $router = $app['router'];
+
+        $resultUrl = $router->getRoutePath('api::duan_get_item', ['hash' => $url->hash]);
+
+        return new JsonResponse($url, Response::HTTP_CREATED, ['Location' => $resultUrl]);
     }
 
     public function get(DuanApp $app, Request $request, $hash)
