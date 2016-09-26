e096ed9d4920caa6e466ae39cf6b495f67319125
Kai Zhang <kai.zhangxa@gmail.com>
2016-09-26 17:13:53 +0200

    Update TokenAuthenticator

--- src/Lib/TokenAuthenticator.php
+++ src/Lib/TokenAuthenticator.php
@@ -1,10 +1,31 @@
 <?php
 namespace Duan\Lib;
 
+use Duan\Models\Token;
+use Duan\Models\User;
+
 class TokenAuthenticator
 {
+    /**
+     * Authenticate by token id.
+     * return corresponding user when found,
+     * false when NOT found.
+     *
+     * @param $id
+     * @return bool|User
+     */
     public function authenticate($id)
     {
-        return !empty(Token::find($id));
+        /** @var Token $token */
+        $token = Token::objects()
+            ->filter('id', '=', $id)
+            ->filter('revoked', '!=', 1)
+            ->single(true);
+
+        if (empty($token)) {
+            return false;
+        } else {
+            return $token->getUser();
+        }
     }
 }
